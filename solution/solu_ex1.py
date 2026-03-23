99bff69197374d74eef6bd705ebca40ec1c55876
JSaidi1 <jabrane_saidi@outlook.com>
2026-03-23 15:32:23 +0100

    solution exercice 1

---
+++ solution/solu_ex1.py
@@ -0,0 +1,50 @@
+"""
+## Exercice 1: Hello World Multilingue
+
+**Énoncé**:
+Créez une application Flask avec une route `/hello/<language>` qui retourne un message "Hello" dans la langue spécifiée.
+
+Exemple d'usage:
+```bash
+curl http://localhost:5000/hello/english
+# {"message": "Hello!", "language": "english"}
+
+curl http://localhost:5000/hello/french
+# {"message": "Bonjour!", "language": "french"}
+```
+"""
+import os
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/hello/<string:language>', methods=['GET'])
+def hello(language):
+
+    if language == "english":
+        return jsonify({
+            "message": f"Hello!",
+            "language": "english",
+            "status": "success"
+        }), 200
+    
+    elif language == "french":
+        return jsonify({
+            "message": f"Bonjour!",
+            "language": "french",
+            "status": "success"
+        }), 200
+    else:
+        return jsonify({
+            "success": False,
+            "error": f"Language {language} do not exists",
+        }), 400
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors"""
+    return jsonify({"error": "Endpoint not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
